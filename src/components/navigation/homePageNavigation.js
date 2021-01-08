2f876b5532d71fa3cfa49bdf5f8f6ad716696d6d
Discode Stu <dstuart85@yahoo.com>
2021-01-07 21:49:48 -0500

    attempted to add confetti lottie animation but doesn't work. changed minor button styles in ariana page

--- src/components/navigation/homePageNavigation.js
+++ src/components/navigation/homePageNavigation.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import saucer from '../../images/alien/saucer.png';
-import galaxy from '../../images/universe/spiral-galaxy.jpg';
-import ariana from '../../images/ariana/pinkAriana.jpg';
-import digweed from '../../images/digweed/psycadelic-digweed.png';
-import einstein from '../../images/banksy/einstein.jpg';
+import React from "react";
+import saucer from "../../images/alien/saucer.png";
+import galaxy from "../../images/universe/spiral-galaxy.jpg";
+import ariana from "../../images/ariana/pinkAriana.jpg";
+import digweed from "../../images/digweed/psycadelic-digweed.png";
+import einstein from "../../images/banksy/einstein.jpg";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 export default function HomePageNavigation() {
   return (
-    <div className='nav-container'>
+    <div className="nav-container">
       <div className="nav-home">
-        <NavLink exact to ="/"  className='home-base-title' activeClassName="nav-link-active">
+        <NavLink
+          exact
+          to="/"
+          className="home-base-title"
+          activeClassName="nav-link-active"
+        >
           Michelle Zoccolillo
         </NavLink>
       </div>
       <div className="nav-alien">
-        <NavLink  to ="/alien" className='nav-alien-link' activeClassName="nav-link-active">
-          <img className='nav-alien-img' src={saucer} />
+        <NavLink
+          to="/alien"
+          className="nav-alien-link"
+          activeClassName="nav-link-active"
+        >
+          <img className="nav-alien-img" src={saucer} />
         </NavLink>
       </div>
       <div className="nav-ariana">
-        <NavLink  to ="/ariana" className='nav-ariana-link' activeClassName="nav-link-active">
-          <img src={ariana} className='nav-ariana-img' />
+        <NavLink
+          to="/ariana"
+          className="nav-ariana-link"
+          activeClassName="nav-link-active"
+        >
+          <img src={ariana} className="nav-ariana-img" />
         </NavLink>
       </div>
       <div className="nav-banksy">
-        <NavLink  to ="/banksy" activeClassName="nav-link-active">
-          <img src={einstein} className='nav-banksy-einstein-img' />
+        <NavLink to="/banksy" activeClassName="nav-link-active">
+          <img src={einstein} className="nav-banksy-einstein-img" />
         </NavLink>
       </div>
       {/* <div className="nav-blog">
@@ -36,13 +49,21 @@ export default function HomePageNavigation() {
         </NavLink>
       </div> */}
       <div className="nav-digweed">
-        <NavLink  to ="/digweed" className='nav-digweed-link' activeClassName="nav-link-active">
-          <img src={digweed} className='nav-digweed-img' />
+        <NavLink
+          to="/digweed"
+          className="nav-digweed-link"
+          activeClassName="nav-link-active"
+        >
+          <img src={digweed} className="nav-digweed-img" />
         </NavLink>
       </div>
       <div className="nav-universe">
-        <NavLink  to ="/universe" className='nav-universe-link' activeClassName="nav-link-active">
-         <img  className='nav-universe-img' src={galaxy} />
+        <NavLink
+          to="/universe"
+          className="nav-universe-link"
+          activeClassName="nav-link-active"
+        >
+          <img className="nav-universe-img" src={galaxy} />
         </NavLink>
       </div>
     </div>
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 
-import centralpark from '../../images/nyc/central-park.jpg'
+import centralpark from "../../images/nyc/central-park.jpg";
 
-import HomePageNavigation from '../navigation/homePageNavigation';
+import HomePageNavigation from "../navigation/homePageNavigation";
+import animation from "../../images/lottie/confetti.json";
+
+import lottie from "lottie-web";
 
 export default function Homepage() {
+  // const container = useRef(null);
+  let animationContainer = React.createRef;
+  React.useEffect(() => {
+    lottie.loadAnimation({
+      container: animationContainer.current,
+      // path: "../../images/lottie/confetti.json",
+      renderer: "json",
+      loop: true,
+      autoplay: true,
+      animationData: animation,
+    });
+  }, []);
+
   return (
-    <div className='home-base'>
-      <img src={centralpark} className='home-base-img' />
-      <HomePageNavigation className='home-base-nav' />
+    <div className="home-base">
+      <img src={centralpark} className="home-base-img" />
+      <HomePageNavigation className="home-base-nav" />
+      <div className="animation-container" ref={animationContainer}></div>
     </div>
   );
 }
